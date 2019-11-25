e2528aa06fa6fcc2ce52f7d420f3134e8bcdfe81
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-25 14:34:27 -0600

    [INIT]

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Partner Activities',
+    'version': '13.0.1.0.0',
+    'author': 'HomebrewSoft',
+    'website': 'https://github.com/HomebrewSoft/partner_activities',
+    'category': 'Operations',
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        'views/mail_activity.xml',
+    ],
+}
