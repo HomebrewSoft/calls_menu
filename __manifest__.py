f19af1cf51fb0d1b5721f52b7e0daae8650b397e
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-27 19:24:06 -0600

    [FIX] act window before search

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Partner Activities',
-    'version': '13.0.2.0.0',
+    'version': '13.0.2.0.1',
     'author': 'HomebrewSoft',
     'website': 'https://github.com/HomebrewSoft/partner_activities',
     'category': 'Operations',
